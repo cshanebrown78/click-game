ae0ad34ed1d2db6b1e2ca65bec3ca198d870fe9d
C Shane Brown <cshanebrown78@gmail.com>
2020-06-07 18:16:31 -0500

    Updates

---
+++ precache-manifest.8d9590b01b70a4e1186630f9fe40859c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "badf0eb7a99bbda801e536596121ddeb",
+    "url": "/cshanebrown78/click-game/index.html"
+  },
+  {
+    "revision": "41d1449c5edaf6d193d9",
+    "url": "/cshanebrown78/click-game/static/css/main.ca87e890.chunk.css"
+  },
+  {
+    "revision": "2bd0f7eaf8daae6a35fc",
+    "url": "/cshanebrown78/click-game/static/js/2.c581d9da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cshanebrown78/click-game/static/js/2.c581d9da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41d1449c5edaf6d193d9",
+    "url": "/cshanebrown78/click-game/static/js/main.7e132b6b.chunk.js"
+  },
+  {
+    "revision": "bfd69396a2c2b215da8a",
+    "url": "/cshanebrown78/click-game/static/js/runtime-main.d337c6d1.js"
+  }
+]);
